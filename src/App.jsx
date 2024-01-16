960fa33e02f17f898d8713b239f11c3a5da2ed15
Gaju <kgaju4730@gmail.com>
2024-01-16 11:23:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nevigation from './Nevigation'
+
+function App() {
+  
+
+  return (
+    <>
+    {/* <h1 className=' bg-red-800 text-white ' >hey i am frontend dev</h1> */}
+    <Nevigation />
+    </>
+  )
+}
+
+export default App
---
+++ src/Nevigation.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+
+const Nevigation = () => {
+  return (
+    <>
+    <div className=" bg-white text-black  flex justify-between  align-center px-5  h-10 ">
+      <div className=" text-red-500 text-lg m-1.5 font-mono cursor-pointer ">
+        Gaju
+
+      </div>
+
+      <div className=" flex  size-9 rounded-md ">
+        <input className=' rounded-full h-10 w-30 ' type="Search hear" value="Search hear...." />
+        
+      </div>
+
+      <ul className=' flex gap-5  m-1.5 cursor-pointer '>
+        <li className='  hover:text-xl hover:text-blue-600  ' >Home</li>
+        <li  className='  hover:text-xl hover:text-blue-600  ' >About</li>
+        <li  className='  hover:text-xl hover:text-blue-600  ' >Blog</li>
+      </ul>
+    </div>
+    <hr />
+
+    <input className=' w-96 rounded-md outline-vi outline-green-500' type="text" name='text'value="hhhh"  />
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nam molestiae consequatur unde enim perspiciatis doloremque veritatis, saepe illo officia neque explicabo necessitatibus odio aut, aperiam, ullam similique porro laboriosam? Quisquam dolores ad at eveniet similique molestias quasi atque. Magnam dolore, enim et, amet, error excepturi laborum maxime dolores expedita minus maiores possimus? Ullam fugiat minus repellat quibusdam sit recusandae praesentium, omnis nam non iure repellendus facilis. Voluptates recusandae saepe cum molestias cumque, odio beatae laboriosam aperiam architecto praesentium asperiores temporibus sunt a porro quasi at. Dolores nisi culpa voluptatibus, sint saepe necessitatibus esse numquam quod animi, a, doloremque repellendus!
+    </p>
+    </>
+  )
+}
+
+export default Nevigation
